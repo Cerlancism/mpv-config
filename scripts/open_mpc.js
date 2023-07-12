db1162a2827a3cf44587e16a1ba3efcd51e1a7f3
Chen Yu <scheduledx@gmail.com>
2023-07-12 22:31:23 +0800

    MPC opening

--- scripts/open_mpc.js
+++ scripts/open_mpc.js
@@ -1,3 +1,5 @@
+var SCREEN_TARGET = 0
+
 function launch_subprocess_with_args() {
     // Pause the video
     mp.set_property_bool("pause", true);
@@ -7,20 +9,59 @@ function launch_subprocess_with_args() {
 
     var timeMS = mp.get_property("time-pos") * 1000
 
+    var mpc_args = [
+        "C:/Program Files/MPC-HC/mpc-hc64.exe",
+        current_file_path,
+        "/start",
+        timeMS.toString(),
+    ]
+
+    var screen = Number(mp.get_property("screen"))
+
+    if (screen === SCREEN_TARGET) {
+        mpc_args.push("/fixedsize")
+        mpc_args.push("1937,1199")
+        mpc_args.push("/monitor")
+        mpc_args.push("1")
+    }
+    else {
+        mpc_args.push("/fixedsize")
+        mpc_args.push("1616,1019")
+    }
+
     // Define your subprocess and arguments
     var subprocess_command = {
-        args: [
-            "C:/Program Files/MPC-HC/mpc-hc64.exe",
-            current_file_path,
-            "/start",
-            timeMS.toString(),
-        ]
+        args: mpc_args,
+        detach: true
     };
-	
-    dump(subprocess_command)
     
     // Launch the subprocess
     var result = mp.utils.subprocess(subprocess_command);
+
+    // dump(result)
+
+    if (screen === SCREEN_TARGET) {
+        setTimeout(function () {
+            var splits = current_file_path.split("/")
+            var caption = splits[splits.length - 1]
+            var cmdow_args = [
+                "C:/Tools/cmdow/bin/Release/cmdow.exe",
+                'Media Player Classic' + "*",
+                "/SIZ",
+                "1937",
+                "1199",
+                "/MOV",
+                "1940",
+                "-680"
+            ]
+
+            // dump(cmdow_args)
+
+            mp.utils.subprocess({
+                args: cmdow_args
+            });
+        }, 100);
+    }
     
     // Uncomment the following line to show the subprocess output in the MPV console (Ctrl+`)
     // mp.msg.info(result.stdout);
