e9335f447602d644d07348da378846038e61a2e1
Chen Yu <scheduledx@gmail.com>
2026-03-05 00:45:25 +0800

    feat: 120fps vf for VR

--- scripts-situational/360plugin.lua
+++ scripts-situational/360plugin.lua
@@ -1,1 +1,8 @@
 mp.command("script-binding mpv360/toggle")
+
+local vf = mp.get_property_native("vf") or {}
+for i = #vf, 1, -1 do
+    if vf[i].name == "fps" then table.remove(vf, i) end
+end
+table.insert(vf, 1, {name="fps", params={fps="120000/1001"}})
+mp.set_property_native("vf", vf)
