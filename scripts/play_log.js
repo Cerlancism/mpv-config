8b055f04eeddbdc5c61c7ad77394cab72acdcfdd
Chen Yu <scheduledx@gmail.com>
2026-03-05 00:01:27 +0800

    Update play_log.js

--- scripts/play_log.js
+++ scripts/play_log.js
@@ -3,13 +3,8 @@ var cleanName = require("../modules/cleanName")
 
 var LOG_PATH = require("../modules/config").LOG_PATH;
 
-//@ts-ignore
-var titleTemplate = mp.get_property("title")
-
-; (function ()
-{
-    if (!LOG_PATH)
-    {
+void (function () {
+    if (!LOG_PATH) {
         print("No logging")
         return
     }
@@ -19,29 +14,26 @@ var titleTemplate = mp.get_property("title")
      */
     var playingList = []
 
-    function getLogName()
-    {
+    function getLogName() {
+        var path = String(mp.get_property("path"))
         var filename = String(mp.get_property("filename/no-ext"))
-        var cleanedName = cleanName(filename)
-		if (filename == cleanedName) {
-			var splits = cleanedName.split("#")
-			cleanedName = splits[0].trim()
-		}
-		return cleanedName
+        var cleanedName = cleanName(path, filename)
+        if (filename == cleanedName) {
+            var splits = cleanedName.split("#")
+            cleanedName = splits[0].trim()
+        }
+        return cleanedName
     }
 
     /**
      * 
      * @param {string} file 
      */
-    function getHistoryFile(file)
-    {
-        try
-        {
+    function getHistoryFile(file) {
+        try {
             return mp.utils.read_file(file)
         }
-        catch (error)
-        {
+        catch (error) {
             return ""
         }
     }
@@ -50,29 +42,22 @@ var titleTemplate = mp.get_property("title")
      * 
      * @param {string} raw 
      */
-    function checkIntegrity(raw)
-    {
-        if (raw === undefined)
-        {
+    function checkIntegrity(raw) {
+        if (raw === undefined) {
             return ""
         }
-        if (raw === "")
-        {
+        if (raw === "") {
             return raw
         }
-        if (raw[raw.length - 1] === "\n")
-        {
+        if (raw[raw.length - 1] === "\n") {
             return raw
         }
-        else
-        {
+        else {
             return raw.split("\n")
-                .filter(function (x)
-                {
+                .filter(function (x) {
                     var splits = x.split(" ")
 
-                    return splits.length == 2 && splits.every(function (y)
-                    {
+                    return splits.length == 2 && splits.every(function (y) {
                         return !isNaN(Number(y))
                     })
                 })
@@ -80,13 +65,11 @@ var titleTemplate = mp.get_property("title")
         }
     }
 
-    mp.register_event("file-loaded", function ()
-    {
+    mp.register_event("file-loaded", function () {
         var cleaned = getLogName()
         var target = LOG_PATH + cleaned + ".dat"
 
-        if (target.length > 128)
-        {
+        if (target.length > 128) {
             print("history name too long")
             return
         }
@@ -95,17 +78,14 @@ var titleTemplate = mp.get_property("title")
 
         var history = checkIntegrity(getHistoryFile(target))
         var playedTime = history.split("\n")
-            .map(function (x)
-            {
-                if (!x)
-                {
+            .map(function (x) {
+                if (!x) {
                     return 0
                 }
                 var parts = x.split(" ")
                 return Number(parts[1]) - Number(parts[0])
             })
-            .reduce(function (a, b)
-            {
+            .reduce(function (a, b) {
                 return a + b
             }, 0)
         var output = history + Date.now()
@@ -115,18 +95,19 @@ var titleTemplate = mp.get_property("title")
         // mp.osd_message(message, 5)
         mp.utils.write_file("file://" + target, output)
 
+        //@ts-ignore
+        var titleTemplate = mp.get_property("title")
+        
         print("Title: " + titleTemplate)
         //@ts-ignore
         mp.set_property("title", titleTemplate + "    " + playTimeMessage)
     })
 
-    mp.register_event("end-file", function ()
-    {
+    mp.register_event("end-file", function () {
         var time = Date.now()
         var last = playingList.shift()
 
-        if (!last)
-        {
+        if (!last) {
             return
         }
 
@@ -137,8 +118,7 @@ var titleTemplate = mp.get_property("title")
 
         // There is least one other session playing the same file simultaniously
         // Abort the end log as it will likely corrupt the log file due to the data race
-        if (lines[lines.length - 1].length === 0)
-        {
+        if (lines[lines.length - 1].length === 0) {
             return
         }
 
