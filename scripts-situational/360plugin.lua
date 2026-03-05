0340d61d07687c60381067eb4378c5377e716067
Chen Yu <scheduledx@gmail.com>
2026-03-06 00:25:04 +0800

    feat: 360plugin scaling if too big

--- scripts-situational/360plugin.lua
+++ scripts-situational/360plugin.lua
@@ -1,8 +1,25 @@
-mp.command("script-binding mpv360/toggle")
+local scale_down = false
+-- local scale_down = true
 
-local vf = mp.get_property_native("vf") or {}
-for i = #vf, 1, -1 do
-    if vf[i].name == "fps" then table.remove(vf, i) end
+local function setup()
+    local w = mp.get_property_native("width") or 0
+    local h = mp.get_property_native("height") or 0
+    mp.msg.info(string.format("360plugin: video size = %dx%d", w, h))
+
+    if h > 4000 and scale_down then
+        mp.msg.info("360plugin: height > 4000, scaling down")
+        mp.set_property("lavfi-complex", "[vid1]scale=iw/2.5:ih/2.5[vo]")
+    else
+        mp.msg.info("360plugin: height within limit, adding fps=120000/1001")
+        mp.set_property("vf", "fps=120000/1001")
+    end
+
+    mp.msg.info("360plugin: activating mpv360")
+    mp.command("script-binding mpv360/toggle")
 end
-table.insert(vf, 1, {name="fps", params={fps="120000/1001"}})
-mp.set_property_native("vf", vf)
+
+if mp.get_property_native("video-params") then
+    setup()
+else
+    mp.register_event("file-loaded", setup)
+end
